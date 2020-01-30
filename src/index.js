2db2d05eb9f0ab645bbdc1758f6fdef5bb20540c
Juan Espinosa <juanespvoid@gmail.com>
2020-01-30 12:07:04 -0700

    step 3

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const list = [];
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/users', (req,res) =>  res.json( list ));
+
+app.post('/users', (req,res) => {
+    list.push({id:0});
+    res.json(user);
+});
+
+app.get('/user/:id', (req,res) => {
+    const user = list.find(usr => usr.id === req.params.id);
+    res.json(user)
+});
+
+app.listen(port, () => console.log('Server is listening on port ${port}'))
